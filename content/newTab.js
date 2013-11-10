a850b5b6cd6d31796698e7191ecd0aa0bcded916
Geoff Lankow <geoff@darktrojan.net>
2013-11-10 15:08:13 +1300

    Switch on donation reminder for version 17

--- content/newTab.js
+++ content/newTab.js
@@ -341,7 +341,7 @@ let newTabTools = {
 
     let oldVersion = newTabTools.prefs.getIntPref("donationreminder");
     let currentVersion = newTabTools.prefs.getIntPref("version");
-    if (oldVersion > 0 && oldVersion < 10) {
+    if (oldVersion > 0 && oldVersion < 17) {
       setTimeout(function() {
         let notifyBox = newTabTools.browserWindow.getNotificationBox(window);
         let label = newTabTools.strings.formatStringFromName("newversion", [currentVersion], 1);
