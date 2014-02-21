4ad7dea4ced5ef65c11a853d781171f0fbbf6d93
Geoff Lankow <geoff@darktrojan.net>
2014-02-22 11:57:40 +1300

    Use different arguments to drawImage when making a thumbnail, fixes #30

--- content/newTab.js
+++ content/newTab.js
@@ -214,8 +214,7 @@ let newTabTools = {
       canvas.width = image.width * scale;
       canvas.height = image.height * scale;
       let ctx = canvas.getContext("2d");
-      ctx.scale(scale, scale);
-      ctx.drawImage(image, 0, 0);
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
 
       canvas.mozFetchAsStream(function(aInputStream) {
         let outputStream = FileUtils.openSafeFileOutputStream(file);
