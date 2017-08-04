b4ad753433bf36985745105f1bc25075b5325b05
Geoff Lankow <geoff@darktrojan.net>
2017-08-05 00:12:19 +1200

    Create our own thumbnails, part 1.1

--- webextension/thumbnail.js
+++ webextension/thumbnail.js
@@ -1,7 +1,7 @@
 {
 	let canvas1 = document.createElement('canvas');
 	canvas1.width = 600;
-	canvas1.height = 300;
+	canvas1.height = 600;
 	let context1 = canvas1.getContext('2d');
 	let scale = canvas1.width / document.documentElement.clientWidth;
 
@@ -10,10 +10,12 @@
 	context1.drawWindow(window, 0, 0, document.documentElement.clientWidth, document.documentElement.clientWidth, '#fff');
 
 	let canvas2 = document.createElement('canvas');
+	canvas2.width = 300;
+	canvas2.height = 300;
 	let context2 = canvas2.getContext('2d');
 
 	context2.imageSmoothingEnabled = true;
-	context2.drawImage(canvas1, 0, 0, 600, 300, 0, 0, 300, 150);
+	context2.drawImage(canvas1, 0, 0, 600, 600, 0, 0, 300, 300);
 
 	canvas2.toBlob(function(blob) {
 		browser.runtime.sendMessage({
